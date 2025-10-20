c609bf504567aa18bbcc36f5870f3eab373998af
Kabeer Jaffri <kabeer11000@gmail.com>
2025-10-20 05:29:28 +0500

    Gearing up for OSS release

--- client@web/next.config.ts
+++ client@web/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 
 const bundleAnalyzer = withBundleAnalyzer({
   enabled: process.env.ANALYZE === "true",
-});
+}); 
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
