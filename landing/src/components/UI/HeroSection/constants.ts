300ce152371012b05a790efbce0dbd930a9f75d9
Kabeer Jaffri <kabeer11000@gmail.com>
2025-11-09 17:45:28 +0500

    feat(Landing): added landing page

--- landing/src/components/UI/HeroSection/constants.ts
+++ landing/src/components/UI/HeroSection/constants.ts
@@ -1,12 +1,12 @@
 // For desktop
-export const phrases = ['Reclaim your documents.', 'Collaborate freely.'];
+export const phrases = ['Reclaim your documents.'];
 export const paragraphPhrases = [
   'An open-source alternative to Google Docs built by developers,',
   'for developers. No vendor lock-in. Just collaboration.',
 ];
 
 // For mobile
-export const mobilePhrases = ['Reclaim your', 'documents. Collaborate.'];
+export const mobilePhrases = ['Reclaim your', 'documents.'];
 export const mobileParagraphPhrases = [
   'An open-source alternative to Google Docs.',
   'No vendor lock-in. Just collaboration.',
